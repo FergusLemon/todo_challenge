43f254551ce910bd90c7b01a6fbcecab816b1974
Fergus Lemon <fergus.lemon@gmail.com>
2015-12-05 12:20:37 +0000

    pass initial controller spec and update html for input box

--- js/client.js
+++ js/client.js
@@ -1,2 +1,12 @@
+function MainController() {
+  this.items = [
+    
+  ]
+}
+
+
+
+
 angular
   .module('ToDoList', ['ngResource'])
+  .controller('MainController', MainController);
--- test/mainController.spec.js
+++ test/mainController.spec.js
@@ -1,11 +1,10 @@
-describe('ToDoListController', function() {
+describe('MainController', function() {
   beforeEach(module('ToDoList'));
 
   var ctrl;
 
   beforeEach(inject(function($controller) {
-    var scope = {}
-    ctrl = $controller('ToDoListController', function($scope) {});
+    ctrl = $controller('MainController', function($scope) {});
   }));
 
   it('initializes with an empty input box and empty to do list', function() {
