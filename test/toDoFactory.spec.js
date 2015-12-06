07ce4d96f29a4ce3dda168ba1df61e4c7f1d76da
Fergus Lemon <fergus.lemon@gmail.com>
2015-12-06 11:17:45 +0000

    create ToDoFactory spec to refactor out some of the responsibilities from the controller

---
+++ test/toDoFactory.spec.js
@@ -0,0 +1,12 @@
+describe('factory: ToDoFactory', function() {
+
+  var toDoFactory;
+
+  beforeEach(module('ToDoList'));
+
+  beforeEach(inject(function(ToDoFactory) {
+    toDoFactory = ToDoFactory;
+  }));
+
+  
+});
