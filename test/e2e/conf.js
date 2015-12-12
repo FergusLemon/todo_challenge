4fc5aab6030e1b0b1c80d06a9a46823e9564e74f
Fergus Lemon <fergus.lemon@gmail.com>
2015-12-12 14:57:08 +0000

    run e2e tests on multiple browsers, include firefox

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -1,4 +1,9 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['toDoListFeature.js']
+  specs: ['toDoListFeature.js'],
+  multiCapabilities: [{
+    'browserName': 'firefox'
+  }, {
+    'browserName': 'chrome'
+  }]
 }
